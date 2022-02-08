6280074e1c6165b399ae4536b92fb03bb65665fc
Samuel74028 <samuel.yenlin@gmail.com>
2022-02-08 10:22:55 -0800

    update

--- space_invader.py
+++ space_invader.py
@@ -7,29 +7,40 @@
 #                                                                 #
 ###################################################################
 #Import library
-import pygame
-import math
+import pygame, math, sys
 
 # Screen constants
-XMODE, YMODE = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 600
 BLACK = (0, 0, 0)
+#Setup Screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 pygame.init()
 
 #Game image constants
 background = pygame.image.load('./picture/starfield.png')
 invader = pygame.image.load('./picture/invader.png')
+spaceship_image = pygame.image.load('./picture/spaceship.png')
+SPACESHIP_WIDTH, SPACESHIP_LENGTH = 55, 40
+spaceship = pygame.transform.scale(spaceship_image, (SPACESHIP_WIDTH, SPACESHIP_LENGTH))
+spaceship.blit(spaceship, (0,0))
 
-#Setup Screen
-screen = pygame.display.set_mode((XMODE, YMODE))
-
+def gameControl():
+   for event in pygame.event.get():
+       spaceship.blit
+       if event.type == pygame.key.get_pressed() and spaceship.x > 0:
+           spaceship.x -= 3
+       if event.type == pygame.key.get_pressed():
+            spaceship.x += 3
+        
 # Game loop
 running = True
 while running:
     # redraw game Screen
     screen.fill((BLACK))
-    screen.blit(background, (0, 0))
-    screen.blit(invader, (100, 100))
+    screen.blit(background, (0,0))
+    gameControl()
     #input events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
