3a0dcdf27a0d7fcb3ce4e29223b95f9f8f762235
Samuel74028 <samuel.yenlin@gmail.com>
2022-02-08 20:59:24 -0800

    new

--- space_invader.py
+++ space_invader.py
@@ -9,11 +9,13 @@
 #Import library
 import pygame, math, sys
 
+
 # Screen constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 600
 BLACK = (0, 0, 0)
 #Setup Screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Space Invader')
 
 
 pygame.init()
@@ -22,27 +24,38 @@ pygame.init()
 background = pygame.image.load('./picture/starfield.png')
 invader = pygame.image.load('./picture/invader.png')
 spaceship_image = pygame.image.load('./picture/spaceship.png')
-SPACESHIP_WIDTH, SPACESHIP_LENGTH = 55, 40
-spaceship = pygame.transform.scale(spaceship_image, (SPACESHIP_WIDTH, SPACESHIP_LENGTH))
-spaceship.blit(spaceship, (0,0))
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT= 55,40
+START_X, START_Y = (SCREEN_WIDTH - SPACESHIP_WIDTH)/2, (SCREEN_HEIGHT)/2
+spaceship = pygame.transform.rotate(spaceship_image, 180)
+spaceship_position = pygame.Rect(START_X, START_Y, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
-def gameControl():
-   for event in pygame.event.get():
-       spaceship.blit
-       if event.type == pygame.key.get_pressed() and spaceship.x > 0:
-           spaceship.x -= 3
-       if event.type == pygame.key.get_pressed():
-            spaceship.x += 3
-        
+
+# Starting Coordinate
+
+def shipControl(event):
+    if event == pygame.K_d:
+        spaceship_position.x += 3 
+        print("Player moved right!")
+    elif event == pygame.K_a:
+        spaceship_position.x -= 3
+        print("Player moved left!")
+    elif event == pygame.K_w:
+        spaceship_position.y += 3
+        print("Player moved up!")
+    elif event == pygame.K_s:
+        spaceship_position.y -= 3
+        print("Player moved down!")
+
 # Game loop
 running = True
 while running:
     # redraw game Screen
-    screen.fill((BLACK))
-    screen.blit(background, (0,0))
-    gameControl()
+    screen.blit(background, (0, 0))
+    screen.blit(spaceship, (spaceship_position.x, spaceship_position.y))
     #input events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            shipControl(event.key)
     pygame.display.update()
