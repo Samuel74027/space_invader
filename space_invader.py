226dc0abf7a88220e2bbc31189c1cdb395824e53
Samuel Lee <samuel.yenlin@gmail.com>
2022-02-09 22:55:00 -0800

    add invader

--- space_invader.py
+++ space_invader.py
@@ -22,7 +22,8 @@ pygame.init()
 
 #Game image constants
 background = pygame.image.load('./picture/starfield.png')
-invader = pygame.image.load('./picture/invader.png')
+invader_image = pygame.image.load('./picture/invader.png')
+invader = pygame.transform.scale(invader_image, (55, 40))
 spaceship_image = pygame.image.load('./picture/spaceship.png')
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT= 55,40
 START_X, START_Y = (SCREEN_WIDTH - SPACESHIP_WIDTH)/2, (SCREEN_HEIGHT)/2
@@ -46,16 +47,26 @@ def shipControl(event):
         spaceship_position.y -= 3
         print("Player moved down!")
 
+x = 500
+delta_x = 0
+y = 500
+def Invader(img, x, y):
+    screen.blit(img, (x, y))
 # Game loop
 running = True
 while running:
     # redraw game Screen
     screen.blit(background, (0, 0))
-    screen.blit(spaceship, (spaceship_position.x, spaceship_position.y))
+    screen.blit(invader, (x, y))
     #input events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            shipControl(event.key)
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                delta_x += 1
+            if event.key == pygame.K_a:
+                delta_x -= 1
+    x += delta_x
     pygame.display.update()
